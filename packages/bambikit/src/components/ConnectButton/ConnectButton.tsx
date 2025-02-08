a314ebe468eedb1f65a48cb3185119520f184b37
BambiMan-DEV <bambi.info@gmail.com>
2025-02-09 00:04:55 +0900

    Add styling and CSS variables for ConnectButton component

--- packages/bambikit/src/components/ConnectButton/ConnectButton.tsx
+++ packages/bambikit/src/components/ConnectButton/ConnectButton.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
+import styles from './ConnectButton.module.css';
 
 export interface ConnectButtonProps {
+  label?: string;
   onClick: () => void;
 }
 
-export const ConnectButton = ({ onClick }: ConnectButtonProps) => {
-  return <button onClick={onClick}>Connect</button>;
+export const ConnectButton = ({ label = 'Connect', onClick }: ConnectButtonProps) => {
+  return <button className={styles.connectButton} onClick={onClick}>{label}</button>;
 };
--- packages/bambikit/src/index.ts
+++ packages/bambikit/src/index.ts
@@ -1,2 +1,3 @@
+import './styles/colors.css';
 export type { ConnectButtonProps } from './components/ConnectButton/ConnectButton';
 export { ConnectButton } from './components/ConnectButton/ConnectButton';
